9b0fa86749f74766e0c412749ed5b91b133270d8
JamieWCGraham <50881444+JamieWCGraham@users.noreply.github.com>
2021-05-31 19:47:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 
+// $("button").on("click",function(event) {
+//   var weight = $("inputs").val();
+//   var height = $("inputs").val();
+//
+//   var BMI = weight/(height**2);
+//
+//   $("h2").toggleClass("new")
+//
+//
+// })
 
-$("button").on("click",function(event){
+const form = document.forms[0];
 
-  console.log(event)
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const { weight, height} = this.elements;
+  // or
+  // const { name, description, task } = event.target.elements;
+  console.log(weight.value,height.value);
+  var weight1 = weight.value;
+  var height1 = height.value;
 
-})
+  var BMI = Math.round(weight1/(height1**2))
 
-console.log("hello")
+   $("h2").text("Your BMI is " + BMI)
+   $("h2").toggleClass("new")
+
+
+   if (BMI < 18.5) {
+     $("h3").text("You are underweight")
+     $("h3").toggleClass("new")
+   }
+
+   if (BMI >18.5   &&  BMI<24.9) {
+
+     $("h3").text("You are healthy weight")
+     $("h3").toggleClass("new")
+
+             }
+   if (BMI > 24.9) {
+     $("h3").text("You are overweight")
+     $("h3").toggleClass("new")
+         }
+      // code block
+
+});
+
+// function runit(weight2, height2){
+//
+//   var weight = weight2;
+//   var height = height2;
+//   console.log(height);
+//   var BMI = weight/(height**2);
+//
+//   $("h2").innerHTML("Your BMI is " + BMI)
+//
+// }
